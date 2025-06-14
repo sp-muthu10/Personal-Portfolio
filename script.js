7687b821ab15712457078d0a4501d939aa783c4b
sp-muthu-10 <p.muthu10112005@gmail.com>
2025-06-14 16:50:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Run when page loads
+console.log("Portfolio loaded successfully!");
+
+// Toggle dark mode function
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
